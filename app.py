9f552e9d03c215cd0d5b40b59510ed9fb9b56c45
Duc LTM <laitrungminhduc@hotmail.com>
2020-07-18 20:12:27 +0700

    Change text in App

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_str = 'Index Page'
+    my_str = 'Hello Azure Vietnam Community'
     my_str = my_str + '<br> Show Request <br>'
     my_str = my_str + '<pre>{}</pre>'.format(request.headers)
     return my_str
